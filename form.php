35e02dd4e8f994ffa0c31c2d1fc32a9467f765cc
Fede Drago <pablo64_4@hotmail.com>
2022-06-17 16:06:58 -0300

    prueba

--- form.php
+++ form.php
@@ -2,10 +2,12 @@
 
 $nombre = $_POST["name"];
 $mail = $_POST["email"];
+$mail = $_POST["tel"];
 $mensaje = $_POST["textarea"];
 
 $mensaje = "Mensaje enviado por " .$nombre .",\r\n";
 $mensaje .= "Su email es: " . $mail ." \r\n";
+$mensaje .= "Su numero celular es es: " . $tel ." \r\n";
 $mensaje .= "Mensaje: " . $_POST["textarea"] . " \r\n";
 
 $para = "msolcelis@gmail.com";
