aa46a969422931a5c1f2c48eb079aeaef90dcf6f
Fede Drago <pablo64_4@hotmail.com>
2022-06-17 15:39:02 -0300

    prueba

--- form.php
+++ form.php
@@ -5,14 +5,14 @@ $mail = $_POST["email"];
 $mensaje = $_POST["textarea"];
 
 $mensaje = "Mensaje enviado por " .$nombre .",\r\n";
-$mensaje = "Su email es " .$email .",\r\n";
-$mensaje = "Mensaje: " .$_POST["mensaje"] .",\r\n";
+$mensaje .= "Su email es: " . $mail ." \r\n";
+$mensaje .= "Mensaje: " . $_POST["mensaje"] . " \r\n";
 
 $para = "msolcelis@gmail.com";
 $asunto = "Mensaje enviado a traves de la web";
 
-mail($para, $asunto, utf8_decode($mensaje), $header );
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("location:tnx.html");
+header("Location:tnx.html");
 
 ?>
